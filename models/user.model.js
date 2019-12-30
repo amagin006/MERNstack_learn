d14646983c57f17a7940dde13fa3399e3bef6e69
Shota Iwamoto <amaginqoo@gmail.com>
2019-12-29 17:45:18 -0800

    Update

---
+++ models/user.model.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const Scheme = mongoose.Schema;
+
+const userSchema = new Scheme({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    minlength: 3
+  },
+},{
+  timestamps: true,
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
